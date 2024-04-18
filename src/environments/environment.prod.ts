0dec86cc7aa94c4ab79075fbe72051fc34d0af37
KAMLESHKUMAR <KAMLESHKNSKUMAWAT@GMAIL.COM>
2024-04-18 12:24:42 +0530

    clarity added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   appVersion: require('../../package.json').version,
-  apiUrl: 'https://fcmpusherapi.kumawat.co.in/v1/',
+  apiUrl: 'https://backend-nodejs-and-mongodb.onrender.com/v1/',
  //apiUrl: 'http://localhost:3000/v1/',
 };
